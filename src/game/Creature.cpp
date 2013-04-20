b01cb9af5b552520624cb38dfe41421635f8876c
Nighoo <nighoo@overninethousand.de>
2013-04-20 19:17:37 +0200

    [002] Fix for quest 5625 "Garments of the light"

--- src/game/Creature.cpp
+++ src/game/Creature.cpp
@@ -405,6 +405,9 @@ bool Creature::UpdateEntry(uint32 Entry, Team team, const CreatureData* data /*=
             SetPvP(false);
     }
 
+    if (GetCreatureInfo()->unit_flags & UNIT_FLAG_PVP || GetCreatureInfo()->unit_flags & UNIT_FLAG_PVP_ATTACKABLE)
+        SetPvP(true);
+
     // Try difficulty dependend version before falling back to base entry
     CreatureTemplateSpells const* templateSpells = sCreatureTemplateSpellsStorage.LookupEntry<CreatureTemplateSpells>(GetCreatureInfo()->Entry);
     if (!templateSpells)
